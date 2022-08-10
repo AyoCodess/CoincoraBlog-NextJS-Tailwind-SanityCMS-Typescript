8496cb1d268ea22a2a9e3596b470ea35f5d96b12
AyoCodess <ayo.daniel.adesanya@gmail.com>
2022-08-10 15:25:44 +0200

    banner v1 complete

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,17 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
-import { Header } from '/components';
+import { Header, Banner } from '/components';
 
 const Home: NextPage = () => {
   return (
-    <div className=''>
+    <div className='max-w-7xl mx-auto'>
       <Head>
         <title>Medium Blog</title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
       <Header /> 
+      <Banner />
     </div>
   );
 };
