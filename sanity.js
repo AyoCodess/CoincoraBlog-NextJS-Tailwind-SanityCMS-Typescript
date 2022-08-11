8a2e87b1b9f21a985533804fd9b0debc02e5449c
AyoCodess <ayo.daniel.adesanya@gmail.com>
2022-08-11 18:22:10 +0200

    deployment bug fix

--- sanity.js
+++ sanity.js
@@ -1,7 +1,7 @@
 import sanityClient from '@sanity/client';
 
 export const client = sanityClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'production',
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
   apiVersion: '2022-08-11', // use current UTC date - see "specifying API version"!
   token: process.env.NEXT_PUBLIC_SANITY_TOKEN,
