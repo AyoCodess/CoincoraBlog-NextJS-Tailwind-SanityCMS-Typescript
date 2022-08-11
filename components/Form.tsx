e1d76de1fa3b7cbca6e912f7efd879a8d6417e44
AyoCodess <ayo.daniel.adesanya@gmail.com>
2022-08-11 14:00:25 +0200

    single post v1  page complete

---
+++ components/Form.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export const Form = () => {
+  return (
+    <form>
+      <label>
+        <span>Name</span>
+        <input type='text' placeholder='Your Name' />
+      </label>
+      <label>
+        <span>Email</span>
+        <input type='text' placeholder='Your Name' />
+      </label>
+      <label>
+        <span>Comment</span>
+        <input type='text' placeholder='Your Name' />
+      </label>
+      <label>
+        <span>Name</span>
+        <input type='text' placeholder='Your Name' />
+      </label>
+    </form>
+  );
+};
--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -1,15 +1,72 @@
 import { GetStaticProps } from 'next';
-import React from 'react';
+import React, { Children } from 'react';
 import { client } from '../../sanity';
 import { PostsData } from '../../typings';
+import imageUrlBuilder from '@sanity/image-url';
+import PortableText from 'react-portable-text';
+import { Form, Header } from '../../components';
 
+// builds image URL from an image object
+const builder = imageUrlBuilder(client);
+const urlFor = (source: object) => builder.image(source);
 interface Props {
   post: PostsData;
 }
 
 const Post = ({ post }: Props) => {
   console.log(post);
-  return <main>game</main>;
+  return (
+    <main>
+      <Header />
+      <img
+        className='w-full h-40 object-cover'
+        src={urlFor(post.mainImage).url()!}
+      />
+      <article className='max-w-3xl mx-auto p-5'>
+        <h1 className='text-3xl mt-10 mb-3'>{post.title}</h1>
+        <h2 className='text-xl font-light text-gray-500 mb-2'>
+          {post.description}
+        </h2>
+
+        <div className='flex items-center space-x-2'>
+          <img
+            className='h-10 w-10 rounded-full'
+            src={urlFor(post.author.image).url()!}
+            alt='author image'
+          />
+          <p className='font-extralight text-sm'>
+            Blog post By{' '}
+            <span className='text-green-600'>{post.author.name}</span> -
+            Published at {new Date(post._createdAt).toLocaleDateString()}
+          </p>
+        </div>
+        <div>
+          <PortableText
+            className='mt-10'
+            content={post.body}
+            dataset={process.env.NEXT_PUBLIC_SANITY_DATASET}
+            projectId={process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}
+            serializers={{
+              h1: (props: any) => (
+                <h1 className='text-2xl font-bold my-5'>{props}</h1>
+              ),
+              h2: (props: any) => (
+                <h2 className='text-xl font-bold my-5'>{props}</h2>
+              ),
+              li: (props: any) => <li className='ml-4 list-disc'>{props}</li>,
+              link: ({ href, children }: any) => (
+                <a className='text-blue-500 hover:underline' href={href}>
+                  {children}
+                </a>
+              ),
+            }}
+          />
+        </div>
+      </article>
+      <hr className='max-w-lg my-5 mx-auto border border-yellow-500' />
+      <Form />
+    </main>
+  );
 };
 
 export default Post;
